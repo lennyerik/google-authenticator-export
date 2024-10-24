50b590201eab59352c0f2fdc069c4a0aab26a906
lennyerik <49204493+lennyerik@users.noreply.github.com>
2024-10-24 14:44:47 +0200

    decoding otp keys

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use base64::Engine;
 use clap::Parser;
+use prost::Message;
+use std::fmt::Write;
+
+const QR_PREFIX: &str = "otpauth-migration://offline?data=";
+const INVALID_DATA_MSG: &str =
+    "Are you sure the QR code is a valid Google Authenticator App export code?";
 
 mod authenticator_export {
     #![allow(clippy::all)]
@@ -29,6 +36,7 @@ fn main() {
             args.image_path.to_string_lossy(),
         ))
     });
+
     let img = img_file.decode().unwrap_or_else(|e| {
         print_error_and_exit(&format!(
             "Failed to read image file '{}': {e}",
@@ -36,10 +44,60 @@ fn main() {
         ))
     });
 
-    let qr_data = match qr_decode::decode_to_bytes(&img.into()) {
-        Ok(data) => data,
-        Err(err) => print_error_and_exit(&format!("Failed to decode QR code: {err}")),
-    };
+    let qr_data = qr_decode::decode_to_string(&img.into())
+        .unwrap_or_else(|e| print_error_and_exit(&format!("Failed to decode QR code: {e}")));
 
-    dbg!(qr_data);
+    let url_data = qr_data
+        .strip_prefix("otpauth-migration://offline?data=")
+        .unwrap_or_else(|| {
+            print_error_and_exit(&format!(
+                "Failed to find {QR_PREFIX} URL in QR code.\n{INVALID_DATA_MSG}"
+            ))
+        });
+
+    let url_decoded_data = urlencoding::decode(url_data).unwrap_or_else(|_| {
+        print_error_and_exit(&format!(
+            "Migration payload URL data contained encoded non-UTF8 characters\n{INVALID_DATA_MSG}"
+        ))
+    });
+
+    let binary_data = base64::prelude::BASE64_STANDARD
+        .decode(url_decoded_data.as_ref())
+        .unwrap_or_else(|e| {
+            print_error_and_exit(&format!(
+                "Failed to parse migration payload data as base64: {e}\n{INVALID_DATA_MSG}"
+            ))
+        });
+
+    let payload = authenticator_export::MigrationPayload::decode(binary_data.as_slice())
+        .unwrap_or_else(|e| {
+            print_error_and_exit(&format!(
+                "Failed to decode binary QR code data: {e}\n{INVALID_DATA_MSG}"
+            ))
+        });
+
+    let contained_account_names =
+        payload
+            .otp_parameters
+            .iter()
+            .fold(String::new(), |mut curr, param| {
+                writeln!(curr, "        {}", param.name).unwrap();
+                curr
+            });
+
+    println!(
+        concat!(
+            "Detected migration payload:\n",
+            "   version: {}\n",
+            "   batch_size: {}\n",
+            "   batch_index: {}\n",
+            "   batch_id: {}\n",
+            "   contained accounts:\n{}"
+        ),
+        payload.version,
+        payload.batch_size,
+        payload.batch_index,
+        payload.batch_id,
+        contained_account_names
+    );
 }
--- src/qr_decode.rs
+++ src/qr_decode.rs
@@ -8,9 +8,12 @@ pub enum Error {
 
     #[error("QR code decoding failed")]
     DecodingFailed,
+
+    #[error("QR code data is not ASCII")]
+    DataNotASCII,
 }
 
-pub fn decode_to_bytes(img: &image::GrayImage) -> Result<Vec<u8>, Error> {
+pub fn decode_to_string(img: &image::GrayImage) -> Result<String, Error> {
     let mut decoder = quircs::Quirc::default();
     let mut codes = decoder
         .identify(img.width() as usize, img.height() as usize, img)
@@ -22,5 +25,5 @@ pub fn decode_to_bytes(img: &image::GrayImage) -> Result<Vec<u8>, Error> {
     }
 
     let data = code.decode().map_err(|_| Error::DecodingFailed)?;
-    Ok(data.payload)
+    String::from_utf8(data.payload).map_err(|_| Error::DataNotASCII)
 }
