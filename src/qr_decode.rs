47148a02219b08cd9dd3654505afbb9fbe836748
lennyerik <49204493+lennyerik@users.noreply.github.com>
2024-10-24 16:10:28 +0200

    unit tests for qr_decode.rs

--- src/qr_decode.rs
+++ src/qr_decode.rs
@@ -1,4 +1,4 @@
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, PartialEq, Eq)]
 pub enum Error {
     #[error("no QR code found")]
     NoQRCodeFound,
@@ -25,5 +25,48 @@ pub fn decode_to_string(img: &image::GrayImage) -> Result<String, Error> {
     }
 
     let data = code.decode().map_err(|_| Error::DecodingFailed)?;
+    dbg!(&data.payload);
     String::from_utf8(data.payload).map_err(|_| Error::DataNotASCII)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    macro_rules! load_img {
+        ($path:literal, $format:ident) => {{
+            let img_bytes = include_bytes!($path);
+            let mut reader = image::ImageReader::new(Cursor::new(img_bytes));
+            reader.set_format(image::ImageFormat::$format);
+            reader.decode().unwrap()
+        }};
+    }
+
+    #[test]
+    fn test_decode_to_string() {
+        let img = load_img!("test_imgs/simple_text_qr_code.gif", Gif);
+        assert_eq!(decode_to_string(&img.into()), Ok("test".into()));
+    }
+
+    #[test]
+    fn test_decode_to_string_not_a_qr_code() {
+        let img = load_img!("test_imgs/not_a_qr_code.jpg", Jpeg);
+        assert_eq!(decode_to_string(&img.into()), Err(Error::NoQRCodeFound));
+    }
+
+    #[test]
+    fn test_decode_to_string_two_qr_codes() {
+        let img = load_img!("test_imgs/two_qr_codes.gif", Gif);
+        assert_eq!(
+            decode_to_string(&img.into()),
+            Err(Error::MultipleQRCodesFound)
+        );
+    }
+
+    #[test]
+    fn test_decode_to_string_non_utf8_qr_code() {
+        let img = load_img!("test_imgs/non_utf8_qr_code.jpg", Jpeg);
+        assert_eq!(decode_to_string(&img.into()), Err(Error::DataNotASCII));
+    }
 }
